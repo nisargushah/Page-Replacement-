8646df9d6aee12015aa2fefa8cff4f3c71325ce2
nisargushah <58926548+nisargushah@users.noreply.github.com>
2020-04-13 05:07:24 -0500

    Update pf.cpp
    FIFO algorithms work, next up LRU

--- pf.cpp
+++ pf.cpp
@@ -7,12 +7,6 @@
 
 using namespace std;
 
-
-struct working_set{
-    int frame1;
-    int frame2;
-    int frame3;
-};
 /*
  
  This Function just prints out the vectors that we passed through it
@@ -34,30 +28,61 @@ void PrintVectors(vector<int> x)
  This Function will be used to count the number of Page Faults usign the FIFO algorithm
  
  */
-void FIFO_page_faults(vector<int> pages_requested, int working_frame_set)
+int FIFO_page_faults(vector<int> pages_requested, int working_frame_number)
 {
-    //
-    int page_faults;
-    working_set FIFO_set;
+    int page_hits;
+    int page_faults = 0 ;
+    int q=0;
+    int total_pages= pages_requested.size();
+    vector<int> working_set;
+    vector<int>::iterator it;
     
-    FIFO_set.frame1 = 10;
-    FIFO_set.frame2 = 10;
-    FIFO_set.frame3 = 10;
-    int i=0;
-    while(i<pages_requested.size())
+    while(!pages_requested.empty())
     {
-        if( pages_requested[i] != FIFO_set.frame1 && pages_requested[i] != FIFO_set.frame2 && pages_requested[i] != FIFO_set.frame3 )
+        int ser = pages_requested[0];
+        it = find (working_set.begin(), working_set.end(), ser);
+        if (it != working_set.end())
         {
-            FIFO_set.frame1 = FIFO_set.frame2;
-            FIFO_set.frame2 = FIFO_set.frame3;
-            FIFO_set.frame3 = pages_requested[i];
-            page_faults++;
+            
+            page_hits++;
+            pages_requested.erase(pages_requested.begin());
         }
-        else if( pages_requested[i] == FIFO_set.frame1 )
+        else
+        {
+            if(working_set.size()>=working_frame_number)
+            {
+                working_set.erase(working_set.begin());
+            }
+            
+            working_set.push_back(pages_requested[0]);
+            pages_requested.erase(pages_requested.begin());
+            page_faults++;
+            
+        }
     }
+    return page_faults;
     
-
+}
+int LRU_page_faults(vector<int> pages_requested, int working_frame_number)
+{
+    int page_hits;
+    int page_faults;
+    vector<int> working_set;
+    vector<int>::iterator it;
     
+    while(!pages_requested.empty())
+    {
+        int ser = pages_requested[0];
+        it = find (working_set.begin(), working_set.end(), ser);
+        if (it != working_set.end())
+        {
+            //Page Hits
+        }
+        else
+        {
+            //Page_faults
+        }
+    }
 }
 
 // MAIN Function.
@@ -97,9 +122,19 @@ int main()
 //    PrintVectors(trial);
     
     int frame_size;
+    int x;
     frame_size = trial[0];
     trial.erase(trial.begin());
     PrintVectors(trial);
     cout<<endl<<endl<<endl<<frame_size;
+    x=FIFO_page_faults(trial,frame_size);
+    cout<<endl<<x;
     
 }
+
+
+
+
+//Working_frame_number is number of frames which we got from the first element of our input file.
+            
+//Working_set is an array with size (working_frame_number) which will store the frames that are supposed to be in memeory.
